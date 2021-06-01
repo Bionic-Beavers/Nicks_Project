b05abb1d5ae18bfd02566b460d2298a4b76b3a31
Nick Reitano <nreitano95@gmail.com>
2021-06-01 10:17:12 -0600

    Refactored Smelly Code

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ app = Flask(__name__)
 
 db_connection = db.connect_to_database()
 
-# Routes 
-
+# Homepage
 @app.route('/', methods=('GET', 'POST'))
 def root():
     
+    # Search Handler
     if request.method == 'POST':
         name = "%" + request.form['name'] + "%"
         query = "SELECT * FROM Organizations WHERE name LIKE %s ORDER BY name" 
@@ -30,38 +30,45 @@ def root():
 
     return render_template("main.j2")
 
+# Method to display search results
 def searchResults(Organizations):
     return render_template("search-results.j2", Organizations=Organizations)
 
+
+# Advanced Search Page
 @app.route('/advanced-search', methods=('GET', 'POST'))
 def advancedSearch():
 
+    # Search Handler
     if request.method == 'POST':
         name = "%" + request.form['name'] + "%"
         
-
-        # Remove the or None
+        # Set query to accept all request form inputs
         try:
-            if request.form['category'] == '' or None:
+            if request.form['category'] == '':
                 category = "%"
             else: 
                 category = request.form['category']
 
             city = "%" + request.form['city'] + "%"
 
-            if request.form['state'] == '' or None:
+            if request.form['state'] == '':
                 state = "%"
             else: 
                 state = request.form['state']
 
+            query = """SELECT * FROM Organizations WHERE
+                        name LIKE %s AND category LIKE %s AND
+                        city LIKE %s AND 
+                        state LIKE %s ORDER BY name""" 
 
-            query = "SELECT * FROM Organizations WHERE name LIKE %s AND category LIKE %s AND city LIKE %s AND state LIKE %s ORDER BY name" 
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(name,category,city,state))
             results = cursor.fetchall()
             cursor.close()
 
             return searchResults(results)
         
+        # Set query to only take the 'name' input
         except: 
 
             query = "SELECT * FROM Organizations WHERE name LIKE %s ORDER BY name" 
@@ -73,6 +80,7 @@ def advancedSearch():
 
     return render_template("advanced-search.j2")
 
+# Browse Page
 @app.route('/browse')
 def browse():
     query = "SELECT * FROM Organizations ORDER BY name;"
@@ -82,6 +90,7 @@ def browse():
 
     return render_template("browse.j2", Organizations=results)
 
+# Helper method for Organization Page
 def get_organization(id):
     query = "SELECT * FROM Organizations WHERE organization_id =" + str(id)
     cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -90,21 +99,22 @@ def get_organization(id):
 
     return organization
 
+# Organization Page
 @app.route('/<int:id>/organization', methods=('GET', 'POST'))
 def organizationPage(id):
+
+    # Get the organization by the organization id
     organization = get_organization(id)
 
-
+    # Set payload as the name of the wikipedia page.
     wikiName = organization['name']
     payload = {"article": wikiName}
 
+    # Call microservice to scrape text summary from wikipedia page by passing in the payload
     output = requests.get('https://hidden-basin-72940.herokuapp.com/', params=payload)
-
     summary = output.text
 
     return render_template("organization-page.j2", organization=organization, summary=summary)
-
-
 
 
 # Listener 
